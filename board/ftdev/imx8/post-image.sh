fd606724b545b3e8d1458a08637f0fc0859fb9fd
furkantokac <furkantokac34@gmail.com>
2020-03-31 09:02:44 +0300

    post-image script path correction.

--- board/ftdev/imx8/post-image.sh
+++ board/ftdev/imx8/post-image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Copy files to output folder
-cp ${BR2_EXTERNAL}/board/ftdev/imx8/custom_files/update.sh ${BINARIES_DIR}/
-cp ${BR2_EXTERNAL}/board/ftdev/imx8/custom_files/boot_firmwares/* ${BINARIES_DIR}/
+cp board/ftdev/imx8/custom_files/update.sh ${BINARIES_DIR}/
+cp board/ftdev/imx8/custom_files/boot_firmwares/* ${BINARIES_DIR}/
